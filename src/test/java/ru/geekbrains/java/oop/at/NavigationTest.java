47bb55b8af2a916eea4fc3bda7d387d830271c62
Alexey Pankratov <pae.86@mail.ru>
2020-07-21 23:27:12 +0300

    Практическое задание скорректировано согласно разобраненому на уроке.

--- src/test/java/ru/geekbrains/java/oop/at/NavigationTest.java
+++ src/test/java/ru/geekbrains/java/oop/at/NavigationTest.java
@@ -1,8 +1,11 @@
 package ru.geekbrains.java.oop.at;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import ru.geekbrains.java.oop.at.base.BaseTest;
 
 public class NavigationTest extends BaseTest {
@@ -18,12 +21,20 @@ public class NavigationTest extends BaseTest {
 //    Тесты
 //            Карьера
 
+    @AfterEach
+    void tearDown() {
+        WebElement header = chromeDriver.findElementByCssSelector("[class*=\"gb-header__content\"]");
+        WebElement footer = chromeDriver.findElementByCssSelector("[class=\"site-footer__content\"]");
+
+        wait15second.until(ExpectedConditions.visibilityOf(
+                header));
+        wait15second.until(ExpectedConditions.visibilityOf(
+                footer));
+    }
+
     @Test
     public void courses() {
-        chromeDriver.findElement(By.cssSelector("[class = gb-header__title]"));
-        chromeDriver.findElement(By.cssSelector("[href=\"/license.pdf\"]"));
-        chromeDriver.findElement(By.cssSelector("[class=\"top-menu-mobile-toggle pull-right js-top-menu-mobile-toggle\"]")).click();
-        chromeDriver.findElement(By.cssSelector("[class=\"nav\"] a[href=\"/courses\"]")).click();
+        chromeDriver.findElement(By.cssSelector("[id=\"nav\"] a[href=\"/courses\"]")).click();
         chromeDriver.findElement(By.cssSelector("button [class=\"svg-icon icon-popup-close-button \"]")).click();
         Assertions.assertEquals(
                 "Курсы",
@@ -33,10 +44,7 @@ public class NavigationTest extends BaseTest {
 
     @Test
     public void topics() {
-        chromeDriver.findElement(By.cssSelector("[class = gb-header__title]"));
-        chromeDriver.findElement(By.cssSelector("[href=\"/license.pdf\"]"));
-        chromeDriver.findElement(By.cssSelector("[class=\"top-menu-mobile-toggle pull-right js-top-menu-mobile-toggle\"]")).click();
-        chromeDriver.findElement(By.cssSelector("[class=\"nav\"] a[href=\"/topics\"]")).click();
+        chromeDriver.findElement(By.cssSelector("[id=\"nav\"] a[href=\"/topics\"]")).click();
         Assertions.assertEquals(
                 "Форум",
                 chromeDriver.findElement(By.cssSelector("[id=\"top-menu\"] h2")).getText()
@@ -45,10 +53,7 @@ public class NavigationTest extends BaseTest {
 
     @Test
     public void posts() {
-        chromeDriver.findElement(By.cssSelector("[class = gb-header__title]"));
-        chromeDriver.findElement(By.cssSelector("[href=\"/license.pdf\"]"));
-        chromeDriver.findElement(By.cssSelector("[class=\"top-menu-mobile-toggle pull-right js-top-menu-mobile-toggle\"]")).click();
-        chromeDriver.findElement(By.cssSelector("[class=\"nav\"] a[href=\"/posts\"]")).click();
+        chromeDriver.findElement(By.cssSelector("[id=\"nav\"] a[href=\"/posts\"]")).click();
         chromeDriver.findElement(By.cssSelector("button [class=\"svg-icon icon-popup-close-button \"]")).click();
         chromeDriver.findElement(By.cssSelector("[class=\"gb-empopup-close\"]")).click();
         Assertions.assertEquals(
@@ -59,10 +64,7 @@ public class NavigationTest extends BaseTest {
 
     @Test
     public void events() {
-        chromeDriver.findElement(By.cssSelector("[class = gb-header__title]"));
-        chromeDriver.findElement(By.cssSelector("[href=\"/license.pdf\"]"));
-        chromeDriver.findElement(By.cssSelector("[class=\"top-menu-mobile-toggle pull-right js-top-menu-mobile-toggle\"]")).click();
-        chromeDriver.findElement(By.cssSelector("[class=\"nav\"] a[href=\"/events\"]")).click();
+        chromeDriver.findElement(By.cssSelector("[id=\"nav\"] a[href=\"/events\"]")).click();
         Assertions.assertEquals(
                 "Вебинары",
                 chromeDriver.findElement(By.cssSelector("[id=\"top-menu\"] h2")).getText()
@@ -71,10 +73,7 @@ public class NavigationTest extends BaseTest {
 
     @Test
     public void tests() {
-        chromeDriver.findElement(By.cssSelector("[class = gb-header__title]"));
-        chromeDriver.findElement(By.cssSelector("[href=\"/license.pdf\"]"));
-        chromeDriver.findElement(By.cssSelector("[class=\"top-menu-mobile-toggle pull-right js-top-menu-mobile-toggle\"]")).click();
-        chromeDriver.findElement(By.cssSelector("[class=\"nav\"] a[href=\"/tests\"]")).click();
+        chromeDriver.findElement(By.cssSelector("[id=\"nav\"] a[href=\"/tests\"]")).click();
         Assertions.assertEquals(
                 "Тесты",
                 chromeDriver.findElement(By.cssSelector("[id=\"top-menu\"] h2")).getText()
@@ -83,10 +82,7 @@ public class NavigationTest extends BaseTest {
 
     @Test
     public void career() {
-        chromeDriver.findElement(By.cssSelector("[class = gb-header__title]"));
-        chromeDriver.findElement(By.cssSelector("[href=\"/license.pdf\"]"));
-        chromeDriver.findElement(By.cssSelector("[class=\"top-menu-mobile-toggle pull-right js-top-menu-mobile-toggle\"]")).click();
-        chromeDriver.findElement(By.cssSelector("[class=\"nav\"] a[href=\"/career\"]")).click();
+        chromeDriver.findElement(By.cssSelector("[id=\"nav\"] a[href=\"/career\"]")).click();
         Assertions.assertEquals(
                 "Карьера",
                 chromeDriver.findElement(By.cssSelector("[id=\"top-menu\"] h2")).getText()
